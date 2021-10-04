10ee934a2cae4f5c604b2b29e6e68d758d6c8a15
Imaan-Abusamra <1839590@students.wits.ac.za>
2021-10-04 21:28:20 +0200

    js scroll button and fixes

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -17,3 +17,15 @@ const burgerMenu = () => {
 document.addEventListener('DOMContentLoaded', () => {burgerMenu()});
 
 
+
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 100){
+        toTop.classList.add("active");
+    } else{
+        toTop.classList.remove("active");
+    }
+})
